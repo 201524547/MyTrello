67dfa10367a52953561877c24986024ad32ccbfa
201524547 <jh.lee@ninehire.com>
2020-10-06 17:10:35 +0900

    Updates

---
+++ precache-manifest.d7d734856f11225dd8633b1c22e2d4ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f04bbf167547447c4c96b881f3b6a0",
+    "url": "/MyTrello/index.html"
+  },
+  {
+    "revision": "d4c6a4348022f0940b91",
+    "url": "/MyTrello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24835b4d5957e9e52a4a",
+    "url": "/MyTrello/static/js/2.91490c30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyTrello/static/js/2.91490c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c6a4348022f0940b91",
+    "url": "/MyTrello/static/js/main.8cf00b07.chunk.js"
+  },
+  {
+    "revision": "c799076bf7cf1ea8dca3",
+    "url": "/MyTrello/static/js/runtime-main.1503c930.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyTrello/static/media/logo.5d5d9eef.svg"
+  }
+]);
